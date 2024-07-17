cd0e4d66610b1699e955fd500d474d1651ce6c45
HUANGEV <HUANGEV6@OOCL.COM>
2024-07-17 11:41:28 +0800

    for openai-translator homework

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -16,12 +16,14 @@ if __name__ == "__main__":
 
     model_name = args.openai_model if args.openai_model else config['OpenAIModel']['model']
     api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
-    model = OpenAIModel(model=model_name, api_key=api_key)
+    base_url = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['base_url']
+    model = OpenAIModel(model=model_name, api_key=api_key, base_url=base_url)
 
 
     pdf_file_path = args.book if args.book else config['common']['book']
     file_format = args.file_format if args.file_format else config['common']['file_format']
+    target_language = args.target_language if args.target_language else config['common']['target_language']
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format)
+    translator.translate_pdf(pdf_file_path, file_format,target_language)
